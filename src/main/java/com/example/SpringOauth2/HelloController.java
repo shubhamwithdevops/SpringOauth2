a48671ed9d95a93b853366c39df2d10778274811
shubhamwithdevops <92227670+shubhamwithdevops@users.noreply.github.com>
2025-07-13 16:37:24 -0700

    Initial commit

---
+++ src/main/java/com/example/SpringOauth2/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.SpringOauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Oauth Security Demo!!!";
+    }
+}
